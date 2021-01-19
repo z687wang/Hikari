d3eaec250848b117a1f345b576ccfdd9f6062a38
Zhe Wang <z687wang@edu.uwaterloo.ca>
2021-01-18 21:34:51 -0500

    Update settings.py

--- backend/hikari/settings.py
+++ backend/hikari/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+&g(df(^by8jvp4v4+ffw$n5+g)j+@h%x7(alo!#c$2(oqoloz'
+SECRET_KEY = # '+&g(df(^by8jvp4v4+ffw$n5+g)j+@h%x7(alo!#c$2(oqoloz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
